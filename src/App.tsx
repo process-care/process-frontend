798f47d7e7f62ca0c97d4bb944f37be633e1be3b
Charlesdoiron <doironcharles@gmail.com>
2021-05-03 15:07:49 +0200

    remove and clean

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,12 @@ import {
   ChakraProvider,
   Box,
   Text,
-  Link,
   VStack,
-  Code,
   Grid,
   theme,
 } from "@chakra-ui/react"
 import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+
 
 export const App = () => (
   <ChakraProvider theme={theme}>
@@ -18,19 +16,10 @@ export const App = () => (
       <Grid minH="100vh" p={3}>
         <ColorModeSwitcher justifySelf="flex-end" />
         <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
           <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
+            .Process
           </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
+        
         </VStack>
       </Grid>
     </Box>

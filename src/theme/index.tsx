8ccdae1f81896e6cf356fee4ecddc67223bdc432
Charlesdoiron <doironcharles@gmail.com>
2021-05-11 09:48:44 +0200

    debug font

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -1,9 +1,14 @@
 import { extendTheme } from "@chakra-ui/react";
 const theme = extendTheme({
+  fonts: {
+    body: "system-ui, sans-serif",
+    heading: "Georgia, serif",
+    mono: "Menlo, monospace",
+  },
   fontSizes: {
     xs: "0.75rem",
     sm: "0.875rem",
-    md: "1rem",
+    md: "3rem",
     lg: "1.125rem",
     xl: "1.25rem",
     "2xl": "1.5rem",
@@ -14,6 +19,42 @@ const theme = extendTheme({
     "7xl": "4.5rem",
     "8xl": "6rem",
     "9xl": "8rem",
+  },
+  fontWeights: {
+    hairline: 100,
+    thin: 200,
+    light: 300,
+    normal: 400,
+    medium: 500,
+    semibold: 600,
+    bold: 700,
+    extrabold: 800,
+    black: 900,
+  },
+  lineHeights: {
+    normal: "normal",
+    none: 1,
+    shorter: 1.25,
+    short: 1.375,
+    base: 1.5,
+    tall: 1.625,
+    taller: "2",
+    "3": ".75rem",
+    "4": "1rem",
+    "5": "1.25rem",
+    "6": "1.5rem",
+    "7": "1.75rem",
+    "8": "2rem",
+    "9": "2.25rem",
+    "10": "2.5rem",
+  },
+  letterSpacings: {
+    tighter: "-0.05em",
+    tight: "-0.025em",
+    normal: "0",
+    wide: "0.025em",
+    wider: "0.05em",
+    widest: "0.1em",
   },
   components: {
     Button: {

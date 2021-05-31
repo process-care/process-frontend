9b901631dc73dd424ab0558f8590bcf6959bbd58
Charlesdoiron <doironcharles@gmail.com>
2021-05-31 10:21:02 +0200

    add wysiwyg

--- src/components/Fields/Wysiwyg.tsx
+++ src/components/Fields/Wysiwyg.tsx
@@ -1,5 +1,24 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import JoditEditor from "jodit-react";
 
-export const Wysiwyg: React.FC = () => {
-  return <p>Wysiwyg</p>;
+export const Wysiwyg: React.F = () => {
+  const editor = useRef(null);
+  const [content, setContent] = useState("");
+
+  const config = {
+    readonly: false, // all options from https://xdsoft.net/jodit/doc/
+  };
+
+  return (
+    <JoditEditor
+      ref={editor}
+      value={content}
+      config={config}
+      tabIndex={1} // tabIndex of textarea
+      onBlur={(newContent: string) => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
+      onChange={(newContent) => {
+        console.log(newContent);
+      }}
+    />
+  );
 };

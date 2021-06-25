ee09d5fe2884ae25b722b2a34c45c9088e4d8766
charles d oiron <doironcharles@gmail.com>
2021-06-25 13:14:10 +0200

    add video integration

--- src/components/CreateSurvey/CreateLanding/Preview/Content/index.tsx
+++ src/components/CreateSurvey/CreateLanding/Preview/Content/index.tsx
@@ -1,8 +1,8 @@
 import React from "react"
-import { Box, Text, Flex } from "@chakra-ui/react"
+import { Box, Text, Flex, Button } from "@chakra-ui/react"
 import { IColors, ILanding } from "interfaces/landing"
 import { Video } from "components/Video"
-
+import { t } from "static/createLanding"
 
 interface Props {
     data: ILanding,
@@ -10,9 +10,13 @@ interface Props {
 }
 
 const placeholder = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt voluptate accusantium ab praesentium enim fuga, unde tempore, libero beatae ratione ea perspiciatis! Blanditiis et, quo velit tenetur labore at reprehenderit.Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt voluptate accusantium ab praesentium enim fuga, unde tempore, libero beatae ratione ea perspiciatis! Blanditiis et, quo velit tenetur labore at reprehenderit."
+const big_placeholder = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt voluptate accusantium ab praesentium enim fuga, unde tempore, libero beatae ratione ea perspiciatis! Blanditiis et, quo velit tenetur labore at reprehenderit.Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt voluptate accusantium ab praesentium enim fuga, unde tempore, libero beatae ratione ea perspiciatis! Blanditiis et, quo velit tenetur labore at reprehenderit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt voluptate accusantium ab praesentium enim fuga, unde tempore, libero beatae ratione ea perspiciatis! Blanditiis et. <br/> <br/> quo velit tenetur labore at reprehenderit.Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt voluptate accusantium ab praesentium enim fuga, unde tempore, libero beatae ratione ea perspiciatis! Blanditiis et, quo velit tenetur labore at reprehenderit.<br/> <br/> Blanditiis et, quo velit tenetur labore at reprehenderit."
 
 export const Content: React.FC<Props> = ({ data, theme }) => {
     const had_media = data.image_cover !== "" || data.video_url !== ""
+    const had_video = data.video_url !== ""
+    const had_image = data.image_cover !== ""
+
     return (
         <Box>
             <Box backgroundColor={theme.base} py="70px" color="white" textAlign="left" px="10%">
@@ -24,12 +28,24 @@ export const Content: React.FC<Props> = ({ data, theme }) => {
                 </Text>
             </Box>
 
-            <Flex p={10}>
-                {had_media && <Box>
-                    <Video url={data.video_url} />
-                    {/* <img src={data.image_cover} style={{ maxWidth: "400px", width: "400px" }} alt="" /> */}
-                </Box>}
-                <Text textAlign="left" w={had_media ? '50%' : "60%"} m={had_media ? "inherit" : "auto"} pl={10} variant="xxs" dangerouslySetInnerHTML={{ __html: data.wysiwyg || placeholder }}></Text>
+            <Flex px="12%" py="10%" >
+                {had_media && (
+                    <Box w="100%">
+                        {had_video && <Video url={data.video_url} />}
+                        {had_image && <img src={data.image_cover} style={{ maxWidth: "400px", width: "400px" }} alt="" />}
+                    </Box>
+                )}
+                <Box w={had_media ? '100%' : "80%"} m={had_media ? "inherit" : "auto"} pl={10}>
+                    <Text textAlign="left" variant="xxs" dangerouslySetInnerHTML={{ __html: data.wysiwyg || big_placeholder }}></Text>
+                    <Flex mt={10} justifyContent="space-between">
+                        <Button variant="rounded" backgroundColor={theme.button}>
+                            {t.cta_participate}
+                        </Button>
+                        <Button variant="rounded" backgroundColor={theme.button}>
+                            {t.cta_show_more}
+                        </Button>
+                    </Flex>
+                </Box>
             </Flex>
         </Box>
 
--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import Iframe from 'react-iframe'
+import ReactPlayer from 'react-player'
 
 interface Props {
     url: string;
@@ -7,11 +7,6 @@ interface Props {
 
 export const Video: React.FC<Props> = ({ url }) => {
     return (
-        <Iframe url={url}
-            width="450px"
-            height="450px"
-            id="myId"
-            className="myClassname"
-            position="relative" />
+        <ReactPlayer url={url} width="100%" />
     )
 }

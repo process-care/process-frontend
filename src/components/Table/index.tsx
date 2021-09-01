5b5dd3cf8911091d469ce9628c65b08cb9c51c78
charles_doiron <doironcharles@gmail.com>
2021-09-01 15:16:30 +0200

    add cursor on hover td

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,6 +1,13 @@
 import * as React from "react";
-import { useTable, Column, useSortBy, HeaderGroup, Row, TableInstance } from "react-table";
-import { Text } from "@chakra-ui/react";
+import {
+  useTable,
+  Column,
+  useSortBy,
+  HeaderGroup,
+  Row,
+  TableInstance,
+} from "react-table";
+import { Box, Text } from "@chakra-ui/react";
 import { useCallback } from "react";
 
 // ---- TYPES
@@ -20,7 +27,7 @@ interface Props {
 const styles = {
   thead: { width: "100%", backgroundColor: "white" },
   tr: { border: "1px solid #e7e7e7" },
-  td: { backgroundColor: "#F6F6F6", padding: "30px" },
+  td: { backgroundColor: "#F6F6F6" },
 };
 
 // ---- COMPONENT
@@ -34,13 +41,22 @@ export const Table: React.FC<Props> = ({ columns, data, onClick }) => {
       <thead style={styles.thead}>
         {headerGroups.map((headerGroup) => (
           <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column, i) => <TableHeader key={i} column={column}/>)}
+            {headerGroup.headers.map((column, i) => (
+              <TableHeader key={i} column={column} />
+            ))}
           </tr>
         ))}
       </thead>
 
       <tbody {...getTableBodyProps()}>
-        {rows.map((row, j) => <TableRow key={j} row={row} prepareRow={prepareRow} onClick={onClick} />)}
+        {rows.map((row, j) => (
+          <TableRow
+            key={j}
+            row={row}
+            prepareRow={prepareRow}
+            onClick={onClick}
+          />
+        ))}
       </tbody>
     </table>
   );
@@ -57,7 +73,7 @@ interface TableHeaderProps {
 const TableHeader = ({ column }: TableHeaderProps) => {
   const props = column.getHeaderProps(column.getSortByToggleProps());
   const content = column.render("Header");
-  const aligned = (content !== 'Titre') ? 'center' : undefined;
+  const aligned = content !== "Titre" ? "center" : undefined;
 
   return (
     <th {...props}>
@@ -69,14 +85,14 @@ const TableHeader = ({ column }: TableHeaderProps) => {
       </Text>
     </th>
   );
-}
+};
 
 // -- Table Row
 
 interface TableRowProps {
-  row: Row,
-  prepareRow: TableInstance['prepareRow'],
-  onClick: ClickAction,
+  row: Row;
+  prepareRow: TableInstance["prepareRow"];
+  onClick: ClickAction;
 }
 
 const TableRow = ({ row, prepareRow, onClick }: TableRowProps) => {
@@ -89,23 +105,20 @@ const TableRow = ({ row, prepareRow, onClick }: TableRowProps) => {
   const click = useCallback(() => onClick(projectId), [projectId, onClick]);
 
   return (
-    <tr
-      {...row.getRowProps()}
-      style={styles.tr}
-      onClick={click}
-    >
+    <tr {...row.getRowProps()} style={styles.tr} onClick={click}>
       {row.cells.map((cell, i) => {
-        const aligned = (i !== 2) ? 'center' : undefined;
+        const aligned = i !== 2 ? "center" : undefined;
 
         return (
-          <td
-            {...cell.getCellProps()}
-            style={styles.td}
-          >
-            <Text variant="current" textAlign={aligned}>{cell.render("Cell")}</Text>
+          <td {...cell.getCellProps()} style={styles.td}>
+            <Box p="30px" _hover={{ cursor: "pointer" }}>
+              <Text variant="current" textAlign={aligned}>
+                {cell.render("Cell")}
+              </Text>
+            </Box>
           </td>
         );
       })}
     </tr>
   );
-}
+};

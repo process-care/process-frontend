7a562fbfcd4c356342a05e5dd8562b6d85354d6c
charles_doiron <doironcharles@gmail.com>
2021-09-09 14:32:16 +0200

    Add consent page

--- src/call/queries/survey/index.ts
+++ src/call/queries/survey/index.ts
@@ -51,6 +51,8 @@ export const GET_SURVEY = gql`
       }
       consentement {
         url
+        id
+        name
       }
       pages {
         id
--- src/pages/Survey/CreateConsent/PdfPreview/index.tsx
+++ src/pages/Survey/CreateConsent/PdfPreview/index.tsx
@@ -1,13 +1,16 @@
+import { Text, Flex, Button } from "@chakra-ui/react";
 import React, { useState } from "react";
-import { Document, Page } from "react-pdf";
+import { Document, Page, pdfjs } from "react-pdf";
 
 interface Props {
   url: string;
 }
 
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
 export const PdfPreview: React.FC<Props> = ({ url }) => {
   const [numPages, setNumPages] = useState(1);
-  const [pageNumber] = useState(1);
+  const [pageNumber, setPageNumber] = useState(1);
 
   const onDocumentLoadSuccess = ({ numPages }: { numPages: number }) => {
     setNumPages(numPages);
@@ -16,11 +19,43 @@ export const PdfPreview: React.FC<Props> = ({ url }) => {
   return (
     <div>
       <Document file={url} onLoadSuccess={onDocumentLoadSuccess}>
-        <Page pageNumber={pageNumber} />
+        <Page pageNumber={pageNumber} height={600} />
       </Document>
-      <p>
-        Page {pageNumber} sur {numPages}
-      </p>
+      <Flex
+        d="flex"
+        justifyContent="space-between"
+        alignItems="center"
+        mt="5px "
+      >
+        {pageNumber !== 1 ? (
+          <Button
+            variant="rounded"
+            backgroundColor="black"
+            p="0"
+            onClick={() => setPageNumber(pageNumber - 1)}
+          >
+            {"<"}
+          </Button>
+        ) : (
+          <p></p>
+        )}
+
+        <Text variant="xs">
+          Page {pageNumber} sur {numPages}
+        </Text>
+        {pageNumber !== numPages ? (
+          <Button
+            variant="rounded"
+            backgroundColor="black"
+            p="0"
+            onClick={() => setPageNumber(pageNumber + 1)}
+          >
+            {">"}
+          </Button>
+        ) : (
+          <p></p>
+        )}
+      </Flex>
     </div>
   );
 };

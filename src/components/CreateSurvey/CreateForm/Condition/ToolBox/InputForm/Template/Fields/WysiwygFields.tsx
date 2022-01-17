7e75f054b8583071579d5090d5d39381ba7aa793
charles_doiron <doironcharles@gmail.com>
2022-01-17 16:38:43 +0100

    wysiwyg

--- src/components/CreateSurvey/CreateForm/Condition/ToolBox/InputForm/Template/Fields/WysiwygFields.tsx
+++ src/components/CreateSurvey/CreateForm/Condition/ToolBox/InputForm/Template/Fields/WysiwygFields.tsx
@@ -1,5 +1,4 @@
 import { Box } from "@chakra-ui/react";
-import { Input } from "components/Fields";
 import { Wysiwyg } from "components/Fields/Wysiwyg";
 
 import React from "react";
@@ -7,16 +6,7 @@ import React from "react";
 export const WysiwygFields: React.FC = () => {
   return (
     <>
-      <Input
-        isCollapsed={false}
-        label="Titre interne de la question"
-        placeholder="Renseigner le nom interne de votre question"
-        name="internal_title"
-        helpText="Ce champ vous permet de donner un titre à la question,il n'est pas visible par les utilisateurs."
-        isRequired
-      />
-
-      <Box w="100%">
+      <Box w="100%" p="5">
         <Wysiwyg id="wysiwyg" />
       </Box>
     </>

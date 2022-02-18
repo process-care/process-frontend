f401a1bd3629a672e6c50ee2f76e380993e68d68
charles_doiron <doironcharles@gmail.com>
2022-02-18 13:03:48 +0100

    deploy

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,9 +1,9 @@
 export const API_URL =
-  process.env.NODE_ENV === 'development'
+  process.env.NODE_ENV === "development"
     ? process.env.REACT_APP_API_URL_GQL_DEV!
     : process.env.REACT_APP_API_URL_GQL_PROD!;
 
 export const API_URL_ROOT =
-  process.env.NODE_ENV === 'development'
+  process.env.NODE_ENV === "development"
     ? process.env.REACT_APP_API_URL_ROOT_DEV!
     : process.env.REACT_APP_API_URL_ROOT_PROD!;

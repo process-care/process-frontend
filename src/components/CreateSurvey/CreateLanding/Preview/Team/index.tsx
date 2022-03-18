2cb71f650e8e243bb2b5d392c636f285c9c8f72e
charlesdoiron <doironcharles@gmail.com>
2022-03-18 11:27:36 +0100

    improve design

--- src/components/CreateSurvey/CreateLanding/Preview/Team/index.tsx
+++ src/components/CreateSurvey/CreateLanding/Preview/Team/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Text, Flex, Container, Image, Grid } from "@chakra-ui/react";
+import { Box, Text, Flex, Image, Grid } from "@chakra-ui/react";
 import { Color, Member as MemberType } from "types/landing";
 import React from "react";
 
@@ -14,15 +14,11 @@ export const Team: React.FC<Props> = ({ members, color_theme, isUserView }) => {
   }
 
   return (
-    <Box pb={10}>
-      <Container variant="hr" maxW="unset" />
-      <Text variant="xlNoMobilVariant" mb="80px" mt="45px">
-        L'équipe
-      </Text>
+    <Box w="100%">
       <Grid
-        mt="40px"
-        templateColumns={["repeat(1, 1fr)", "repeat(1, 1fr)", "repeat(2, 1fr)", "repeat(4, 1fr)"]}
-        gap="40px 10px"
+        pt="40px"
+        templateColumns={["repeat(2, 1fr)", "repeat(2, 1fr)", "repeat(2, 1fr)", "repeat(4, 1fr)"]}
+        gap="5px"
       >
         {members.map(({ job, name, image }: any, i: number) => (
           <Member isUserView={isUserView} key={i} job={job} name={name} image={image} color={color_theme?.base} />
@@ -32,13 +28,13 @@ export const Team: React.FC<Props> = ({ members, color_theme, isUserView }) => {
   );
 };
 
-const Member: React.FC<MemberType> = ({ job, name, image, color, isUserView }) => {
+const Member: React.FC<MemberType> = ({ job, name, image, color }) => {
   return (
-    <Flex flexDirection="column" justifyContent="center">
+    <Flex flexDirection="column" justifyContent="center" textAlign="center" mb="30px">
       <Image
         fallbackSrc={`https://via.placeholder.com/150/${color?.replace("#", "")}/${color?.replace("#", "")}`}
         borderRadius="full"
-        boxSize={isUserView ? "230px" : "80px"}
+        boxSize="100px"
         src={image}
         alt={name}
         mx="auto"

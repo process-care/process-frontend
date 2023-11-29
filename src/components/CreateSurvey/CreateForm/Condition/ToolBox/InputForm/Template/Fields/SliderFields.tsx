c6a63182193f165f2c1d2864594ba6ec7bbb5958
Mikaël <mikle@hey.com>
2023-11-29 11:12:39 +0100

    fix: correct reversed sliders + disable vertical option

--- src/components/CreateSurvey/CreateForm/Condition/ToolBox/InputForm/Template/Fields/SliderFields.tsx
+++ src/components/CreateSurvey/CreateForm/Condition/ToolBox/InputForm/Template/Fields/SliderFields.tsx
@@ -56,7 +56,7 @@ export default function SliderFields(): JSX.Element {
           <Box w="45%"></Box>
         </Flex>
 
-        <Switch label="Slider en vue verticale" id="vertical" defaultChecked={false} />
+        {/* <Switch label="Slider en vue verticale" id="vertical" defaultChecked={false} /> */}
 
         <Box mb={4} />
 
